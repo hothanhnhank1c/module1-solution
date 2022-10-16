eb094604fd1df86b22215b75ac2f2c5e080b1a59
hothanhnhank1c <37531502+hothanhnhank1c@users.noreply.github.com>
2022-10-16 19:35:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-(function(){
+(function() {
     'use strict';
-    angular.module('myApp', []).controller('LunchCheckController',LunchCheckController);
-    LunchCheckController.$inject = ['$scope', '$filter'];
-    function LunchCheckController($scope,$filter){
-        $scope.lunchMenu = ''
-        $scope.submitForm = function () {
-            if($scope.lunchMenu !== ''){
-                $scope.listMenu = $scope.lunchMenu.split(',');
-                if($scope.listMenu.length <= 3){
-                    $scope.message = 'Enjoy!'
-                }else{
-                    $scope.message = 'Too much!'
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
+
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arrayDishesWithoutEmptys.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
                 }
-            }else{
-                $scope.message = 'Please enter data first'
             }
         };
     }
